5fb8333616f4862b5b910f446584228655f52c2f
zerogvt <gkoumplias@gmail.com>
2021-01-17 21:46:17 +0200

    settings + set route

--- main.go
+++ main.go
@@ -6,13 +6,30 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+// Settings is our
+type Settings struct {
+	ErrorRate     int `json:"err"`
+	ReqsPerMinute int `json:"rpm"`
+}
+
+var defSet = Settings{
+	ErrorRate:     10,
+	ReqsPerMinute: 600}
+
+var settings *Settings
+
+func init() {
 	rand.Seed(time.Now().UnixNano())
+	settings = &defSet
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	v := rand.Intn(100)
-	if v > 90 {
+	if v < settings.ErrorRate {
 		http.Error(w, http.StatusText(http.StatusInternalServerError),
 			http.StatusInternalServerError)
 		return
@@ -20,9 +37,24 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hello")
 }
 
+func settingsHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	v, ok := q["err"]
+	if ok {
+		settings.ErrorRate, _ = strconv.Atoi(v[0])
+	}
+	v, ok = q["rpm"]
+	if ok {
+		settings.ReqsPerMinute, _ = strconv.Atoi(v[0])
+	}
+	io.WriteString(w, fmt.Sprintf("error rate: %d, requests per minute: %d",
+		settings.ErrorRate, settings.ReqsPerMinute))
+}
+
 func client() {
+	sleepT := time.Millisecond * time.Duration(60000/settings.ReqsPerMinute)
 	for {
-		time.Sleep(1 * time.Second)
+		time.Sleep(sleepT)
 		r, err := http.Get("http://localhost:8000/")
 		if err == nil {
 			fmt.Println(r.StatusCode)
@@ -33,8 +65,8 @@ func client() {
 }
 
 func main() {
-	fmt.Println("hello")
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/set/", settingsHandler)
 	go client()
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
